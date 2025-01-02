19c9ee8b4d39c9be9c0876129aaa772e45ee5a39
Ghanshyam <ghanshyamsolanki1612@gmail.com>
2025-01-02 09:34:00 +0530

    update user model

--- app/Models/User.php
+++ app/Models/User.php
@@ -6,12 +6,11 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
-use Spatie\Permission\Traits\HasRoles;
 use Filament\Models\Contracts\FilamentUser;
 use Filament\Panel;
 class User extends Authenticatable implements FilamentUser
 {
-    use HasFactory, Notifiable, HasRoles;
+    use HasFactory, Notifiable;
 
 
     public function canAccessPanel(Panel $panel): bool
