89317ebb4a7964e93d3d0e048935152f002a6275
Ghanshyam <ghanshyamsolanki1612@gmail.com>
2025-01-02 20:48:31 +0530

    update diamond resource image and video input

--- app/Filament/Widgets/CustomersChart.php
+++ app/Filament/Widgets/CustomersChart.php
@@ -12,19 +12,18 @@ class CustomersChart extends ChartWidget
 
     protected function getData(): array
     {
-        // $customersPerMonth = Customer::selectRaw('MONTH(created_at) as month, COUNT(*) as count')
-        //     ->whereYear('created_at', date('Y'))
-        //     ->groupBy('month')
-        //     ->orderBy('month')
-        //     ->pluck('count')
-        //     ->toArray();
+        $customersPerMonth = Customer::selectRaw('MONTH(created_at) as month, COUNT(*) as count')
+            ->whereYear('created_at', date('Y'))
+            ->groupBy('month')
+            ->orderBy('month')
+            ->pluck('count')
+            ->toArray();
 
         return [
             'datasets' => [
                 [
                     'label' => 'Customers',
-                    'data' => [4344, 5676, 6798, 7890, 8987, 9388, 10343, 10524, 13664, 14345, 15753, 17332],
-                    // 'data' => array_pad($customersPerMonth, 12, 0), // Pad with zeros for months with no customers
+                    'data' => array_pad($customersPerMonth, 12, 0), 
                     'fill' => 'start',
                 ],
             ],
