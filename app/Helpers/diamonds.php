7498700063c05a4fb6d03a1228f3c5b1edf54283
Ghanshyam_1612 <ghanshyamsolanki1612@gmail.com>
2025-03-17 16:45:59 +0530

    update diamond helpers class

--- app/Helpers/diamonds.php
+++ app/Helpers/diamonds.php
@@ -27,7 +27,7 @@ class diamonds
                             'cut' => empty($row[6]) ? null : $row[6],
                             'color' => $row[7] ?? null,
                             'clarity' => $row[8] ?? null,
-                            'images' => $row[9] ?? null,
+                            'image_link' => $row[9] ?? null,
                             'size' => $row[10] ?? null,
                             'l_w_ratio' => $row[11] ?? null,
                             'table' => $row[12] ?? null,
