4aadf7f38e421648cc5e9b41f1d2b31f7aafbda4
shaileshmakwana099 <121852572+shaileshmakwana099@users.noreply.github.com>
2025-04-23 20:45:03 +0530

    Fix Design
    - Card page fix design
    - Fix wishlist icon
    - change footer design

--- resources/views/components/antique-cut-diamonds.blade.php
+++ resources/views/components/antique-cut-diamonds.blade.php
@@ -3169,7 +3169,7 @@
                                         <div class="relative flex flex-col items-center gap-3">
                                             <!-- Wishlist Heart Icon -->
                                             <button id="wishlist-btn-{{ $diamond->id }}"
-                                                class="absolute top-2 right-2 p-2 rounded-full transition-colors duration-300"
+                                                class="absolute -top-2 right-2 p-2 rounded-full transition-colors duration-300"
                                                 onclick="toggleWishlist({{ $diamond->id }})">
                                                 <svg id="wishlist-icon-{{ $diamond->id }}"
                                                     xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"
